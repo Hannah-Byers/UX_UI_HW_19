83d19df777650162cdf966529d3733d5642ff6bc
Hannah-Byers <hanhanbyers@gmail.com>
2023-12-11 16:25:01 -0600

    Created buttons and hover

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // jQuery code for the button interaction
+    $('#viewProjectButton').click(function() {
+        $(this).css('background-color', 'pink'); // Change background color to pink on click
+    });
+});
